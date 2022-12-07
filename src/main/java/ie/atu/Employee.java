b095035a41faeb9e043bd414fba146e18f9d048d
G00386395 <g00386395@atu.ie>
2022-12-07 10:20:03 +0000

    Name feature

--- src/main/java/ie/atu/Employee.java
+++ src/main/java/ie/atu/Employee.java
@@ -1,4 +1,65 @@
 package ie.atu;
 
 public class Employee {
+
+    String name;
+    String PPS;
+    String gender;
+    String employment;
+    int age;
+
+    public Employee(String name, String PPS, String gender, String employment, int age){
+        if(name.length()>3 && name.length()<25){
+            System.out.println("Valid name");
+            this.name = name;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid name");
+        }
+
+//        if(PPS.length()==11){
+//            System.out.println("Valid ID");
+//        }
+//        else{
+//            throw new IllegalArgumentException("Invalid PPS ID");
+//        }
+//
+//        if(employment.equals("Full-Time") || employment.equals("Part-Time") || employment.equals("Contract")){
+//            System.out.println("Valid Employment");
+//        }
+//        else{
+//            throw new IllegalArgumentException("Invalid Employment");
+//        }
+//
+//        if(gender.equals("Female") || gender.equals("Male") || gender.equals("Non-Binary")) {
+//            System.out.println("Valid Gender");
+//        }
+//        else{
+//            throw new IllegalArgumentException("Invalid Gender");
+//        }
+//
+//        if(age >= 16){
+//            System.out.println("Valid age");
+//            this.age = age;
+//        }
+//        else{
+//            throw new IllegalArgumentException("Invalid Age, must be over 16");
+//        }
+    }
+
+//    public String getPPS() {
+//        return PPS;
+//    }
+//    public String getGender() {
+//        return gender;
+//    }
+//    public String getEmployment() {
+//        return employment;
+//    }
+//    public int getAge() {
+//        return age;
+//    }
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -2,6 +2,7 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +11,70 @@ class EmployeeTest {
     @BeforeEach
     void setUp() {
     }
+
+    @Test
+    void validName(){
+        Employee myEmployee = new Employee("Cliodhna", "12345678913", "Female", "Full-Time", 56);
+        assertEquals("Cliodhna", myEmployee.getName());
+    }
+
+    @Test
+    void invalidName(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{new Employee("Al", "12345678912",
+                "Female", "Full-Time", 56);});
+        assertEquals("Invalid name", exception.getMessage());
+    }
+
+//    @Test
+//    void validID(){
+//        Employee myEmployee = new Employee("Cliodhna", "12345678912", "Female", "Full-Time", 56);
+//        assertEquals("123456789123", myEmployee.getPPS());
+//    }
+//
+//    @Test
+//    void invalidID(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, () ->{new Employee("Cliodhna", "123456789",
+//                "Female", "Full-Time", 56);});
+//        assertEquals("Invalid PPS ID", exception.getMessage());
+//    }
+//
+//    @Test
+//    void validEmployment(){
+//
+//    }
+//
+//    @Test
+//    void invalidEmployment(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, () ->{new Employee("Cliodhna", "12345678912",
+//                "Female", "Student", 56);});
+//        assertEquals("Invalid Employment", exception.getMessage());
+//    }
+//
+//    @Test
+//    void validGender(){
+//        Employee myEmployee = new Employee("Cliodhna", "12345678912", "Female", "Full-Time", 22);
+//        assertEquals("Female", myEmployee.getGender());
+//    }
+//
+//    @Test
+//    void invalidGender(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, () ->{new Employee("Cliodhna", "12345678913",
+//                "Dog", "Part-Time", 56);});
+//        assertEquals("Invalid Gender", exception.getMessage());
+//    }
+//
+//    @Test
+//    void validAge(){
+//        Employee myEmployee = new Employee("Cliodhna", "12345678912", "Female", "Full-Time", 22);
+//        assertEquals(22, myEmployee.getAge());
+//    }
+//
+//    @Test
+//    void invalidAge(){
+//        Exception exception = assertThrows(IllegalArgumentException.class, () ->{new Employee("Cliodhna", "12345678913",
+//                "Female", "Part-Time", 12);});
+//        assertEquals("Invalid Age, must be over 16", exception.getMessage());
+//    }
 
     @AfterEach
     void tearDown() {
